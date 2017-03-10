74f84e98d03e17dcaba6b3e232cc94d8e7d83094
Thomas Boerger <thomas@webhippie.de>
2017-03-10 09:40:49 +0100

    Make it possible to disable local caching

--- rootfs/etc/owncloud.d/65-caching.sh
+++ rootfs/etc/owncloud.d/65-caching.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-echo "Configure caching..."
-occ config:system:set memcache.local --value "${OWNCLOUD_CACHING_CLASS}"
+if [[ ${OWNCLOUD_CACHING_CLASS} == "false" ]]
+then
+  echo "Disable caching..."
+  occ config:system:delete memcache.local
+else
+  echo "Configure caching..."
+  occ config:system:set memcache.local --value "${OWNCLOUD_CACHING_CLASS}"
+fi
 
 true
