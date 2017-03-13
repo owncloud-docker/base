e6981cc082edc32b7d5d86f6338b64d7622e5976
Thomas Boerger <thomas@webhippie.de>
2017-03-13 09:46:51 +0100

    Fixed caching class naming

--- rootfs/etc/entrypoint.d/45-caching.sh
+++ rootfs/etc/entrypoint.d/45-caching.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 declare -x OWNCLOUD_CACHING_CLASS
-[[ -z "${OWNCLOUD_CACHING_CLASS}" ]] && OWNCLOUD_CACHING_CLASS="\OC\Memcache\APCu"
+[[ -z "${OWNCLOUD_CACHING_CLASS}" ]] && OWNCLOUD_CACHING_CLASS="\\OC\\Memcache\\APCu"
