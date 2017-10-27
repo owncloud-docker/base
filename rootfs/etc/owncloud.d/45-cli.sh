01e31cdb57631a20cf8a809aa8422afd44cde1b0
Thomas Boerger <thomas@webhippie.de>
2017-10-27 14:54:23 +0200

    Always disable updatechecker

--- rootfs/etc/owncloud.d/45-cli.sh
+++ rootfs/etc/owncloud.d/45-cli.sh
@@ -3,4 +3,7 @@
 echo "Enforcing cli url..."
 occ config:system:set overwrite.cli.url --value http://localhost${OWNCLOUD_SUB_URL}
 
+echo "Disabling update checks..."
+occ config:system:set updatechecker --value false
+
 true
