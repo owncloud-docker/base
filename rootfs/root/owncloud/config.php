1c2a6cadf3301d43b681a25428ea3373fb7aae60
Vivek Khurana <vivek@microsoft.com>
2018-02-08 09:51:25 -0800

    cr updates

--- rootfs/root/owncloud/config.php
+++ rootfs/root/owncloud/config.php
@@ -3,7 +3,6 @@ $CONFIG = array(
   "datadirectory" => "${OWNCLOUD_VOLUME_FILES}",
   "updatechecker" => false,
   "upgrade.disable-web" => true,
-  "files_external_allow_create_new_local" => "${OWNCLOUD_ALLOW_EXTERNAL_LOCAL_STORAGE}",
   "apps_paths" => array(
     0 => array(
       "path" => OC::$SERVERROOT . "/apps",
