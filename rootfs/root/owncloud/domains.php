92d6a6b64755a118bc17d97538ebffe6c1549d0e
Thomas Boerger <thomas@webhippie.de>
2018-03-23 22:18:10 +0100

    Respect HTTP_X_FORWARDED_HOST while detecting a trusted domain

--- rootfs/root/owncloud/domains.php
+++ rootfs/root/owncloud/domains.php
@@ -1,6 +1,17 @@
 <?php
+
+if (isset($_SERVER["HTTP_X_FORWARDED_HOST"])) {
+  $domain = $_SERVER["HTTP_X_FORWARDED_HOST"];
+} else if (isset($_SERVER["SERVER_NAME"])) {
+  $domain = $_SERVER["SERVER_NAME"];
+} else {
+  $domain = "localhost";
+}
+
 $CONFIG = array(
   "trusted_domains" => array(
-    0 => isset($_SERVER["SERVER_NAME"]) ? $_SERVER["SERVER_NAME"] : "localhost"
+    0 => $domain
   )
 );
+
+var_dump($CONFIG);
