dea62b891de51a5d644fc5fd4e3c66295b0394a4
Patrick Jahns <github@patrickjahns.de>
2018-04-30 20:10:50 -0600

    fix hook scripts

--- rootfs/etc/owncloud.d/25-chown.sh
+++ rootfs/etc/owncloud.d/25-chown.sh
@@ -5,10 +5,10 @@ then
   echo "Skipping chown as requested..."
 else
   echo "Fixing hook perms..."
-  find ${OWNCLOUD_PRE_INSTALL_PATH} -iname *.sh | xargs -r -0 chmod +x
-  find ${OWNCLOUD_POST_INSTALL_PATH} -iname *.sh | xargs -r -0 chmod +x
-  find ${OWNCLOUD_PRE_SERVER_PATH} -iname *.sh | xargs -r -0 chmod +x
-  find ${OWNCLOUD_POST_SERVER_PATH} -iname *.sh | xargs -r -0 chmod +x
+  find ${OWNCLOUD_PRE_INSTALL_PATH} -iname *.sh  -print0 | xargs -r -0 chmod +x
+  find ${OWNCLOUD_POST_INSTALL_PATH} -iname *.sh -print0 | xargs -r -0 chmod +x
+  find ${OWNCLOUD_PRE_SERVER_PATH} -iname *.sh -print0 | xargs -r -0 chmod +x
+  find ${OWNCLOUD_POST_SERVER_PATH} -iname *.sh -print0 | xargs -r -0 chmod +x
 
   echo "Fixing base perms..."
   find /var/www/owncloud \( \! -user www-data -o \! -group www-data \) -print0 | xargs -r -0 chown www-data:www-data
