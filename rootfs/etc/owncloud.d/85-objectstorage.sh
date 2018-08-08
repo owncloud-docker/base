2434fafe04f1a0887f4ea130234fbb721bac4f6d
Piotr Mrowczynski <mrow4a@yahoo.com>
2018-08-08 09:43:02 +0200

    Add option to create s3 bucket, as autocreation is depreciated

--- rootfs/etc/owncloud.d/85-objectstorage.sh
+++ rootfs/etc/owncloud.d/85-objectstorage.sh
@@ -6,6 +6,12 @@ then
   envsubst \
     '${OWNCLOUD_OBJECTSTORE_CLASS} ${OWNCLOUD_OBJECTSTORE_BUCKET} ${OWNCLOUD_OBJECTSTORE_AUTOCREATE} ${OWNCLOUD_OBJECTSTORE_VERSION} ${OWNCLOUD_OBJECTSTORE_REGION} ${OWNCLOUD_OBJECTSTORE_KEY} ${OWNCLOUD_OBJECTSTORE_SECRET} ${OWNCLOUD_OBJECTSTORE_ENDPOINT} ${OWNCLOUD_OBJECTSTORE_PATHSTYLE}' \
       < /root/owncloud/objectstore.php > /var/www/owncloud/config/objectstore.config.php
+
+  if [[ ${OWNCLOUD_OBJECTSTORE_CREATE_BUCKET} == "true" ]]
+  then
+    echo "Creating s3 bucket"
+    occ s3:create-bucket ${OWNCLOUD_OBJECTSTORE_BUCKET} --accept-warning
+  fi
 else
   echo "Removing object storage..."
   rm -f /var/www/owncloud/config/objectstore.config.php
