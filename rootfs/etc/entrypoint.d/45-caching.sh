6e9832ee20344c2dc0d0abc0b8d746fb57335a73
Thomas Boerger <thomas@webhippie.de>
2018-10-09 11:50:18 +0200

    Refactoring of entire structure, new base image
    * Added
      * Added scripts folder before and after cronjob
    * Changed
      * Change to environment-based configuration
      * Reduce amount of shell script magic
      * Use the new `owncloud/php` Docker image
      * Combined `owncloud-*` scripts to `owncloud`
      * Moved templates to `/etc/templates`
      * Reduced amount of required templates
    * Removed
      * Removed Apache configuration, part of base image now
      * Removed `/usr/local/bin` scripts, moved to `/usr/bin`

--- rootfs/etc/entrypoint.d/45-caching.sh
+++ rootfs/etc/entrypoint.d/45-caching.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-declare -x OWNCLOUD_CACHING_CLASS
-[[ -z "${OWNCLOUD_CACHING_CLASS}" ]] && OWNCLOUD_CACHING_CLASS="\\OC\\Memcache\\APCu"
+declare -x OWNCLOUD_MEMCACHE_LOCAL
+[[ -z "${OWNCLOUD_MEMCACHE_LOCAL}" ]] && OWNCLOUD_MEMCACHE_LOCAL="${OWNCLOUD_CACHING_CLASS:-\\OC\\Memcache\\APCu}"
 
 true
