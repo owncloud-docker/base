141327210a84f43419c11b953cad8a68a8284c4e
Thomas Boerger <thomas@webhippie.de>
2019-11-01 19:55:34 +0100

    Make sure to update environment only as root group
    Otherwise it fails hard to do that

--- v18.04/overlay/etc/entrypoint.d/98-overwrite.sh
+++ v18.04/overlay/etc/entrypoint.d/98-overwrite.sh
@@ -8,7 +8,9 @@ export APACHE_ACCESS_LOG="${OWNCLOUD_ACCESSLOG_LOCATION:-/dev/stdout}"
 export APACHE_DOCUMENT_ROOT="${APACHE_DOCUMENT_ROOT:-/var/www/owncloud}"
 export APACHE_LISTEN="${APACHE_LISTEN:-8080}"
 
-# Write env to file for crond
-env >| /etc/environment
+if [[ "$(id -gn)" == "root" ]]
+then
+  env >| /etc/environment
+fi
 
 true
