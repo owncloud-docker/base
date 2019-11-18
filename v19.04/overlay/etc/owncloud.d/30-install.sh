ac4829e14fd3fd4bb76204e540dd6e710f34a2f0
Thomas Boerger <thomas@webhippie.de>
2019-11-18 13:36:42 +0100

    Add env variable OWNCLOUD_APPS_DEPRECATED
    To disable deprecated apps before a version upgrade we are introducing
    this new env variables. This way the admin can control which app gets
    properly disabled before an upgrade gets applies.
    
    This is especially usefull for upgrading 10.2.x to 10.3.x since gallery
    and files_videoplayer won't get bundles with the core tarball anymore
    and the latter one is also not published to the marketplace which could
    result in a broken upgrade process. Add an env variable like
    OWNCLOUD_APPS_DEPRECATED=gallery,files_videoplayer to the container
    start and it will be disabled properly.

--- v19.04/overlay/etc/owncloud.d/30-install.sh
+++ v19.04/overlay/etc/owncloud.d/30-install.sh
@@ -10,6 +10,15 @@ fi
 
 if /usr/bin/owncloud installed
 then
+  for VAL in $(echo ${OWNCLOUD_APPS_DEPRECATED} | tr "," " ")
+  do
+    if /usr/bin/occ app:list -nq ${VAL} | grep -q Enabled
+    then
+      echo "Disabling deprecated ${VAL}..."
+      /usr/bin/occ app:disable ${VAL}
+    fi
+  done
+
   echo "Upgrading server database..."
   /usr/bin/owncloud migrate
 else
