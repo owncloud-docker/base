274e0b5714ef9b0e69c2be2ea6119f4773bdfebe
Robert Kaussow <mail@geeklabor.de>
2021-05-14 12:34:54 +0200

    chore: configure nss-wrapper

---
+++ v20.04/overlay/etc/entrypoint.d/5-nsswrapper.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+HOME=/home/owncloud
+
+grep -v -e ^owncloud -e ^$USER_ID /etc/passwd > "$HOME/passwd"
+echo "owncloud:x:${USER_ID}:${GROUP_ID}:ownCloud Application User:${HOME}:/sbin/nologin" >> "$HOME/passwd"
+
+export LD_PRELOAD=libnss_wrapper.so
+export NSS_WRAPPER_PASSWD=${HOME}/passwd
+export NSS_WRAPPER_GROUP=/etc/group
