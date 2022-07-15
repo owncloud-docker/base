3f67d408dce653677acd20934a5a67bf68a6c834
Robert Kaussow <mail@thegeeklab.de>
2022-07-15 09:53:02 +0200

    fix: respect the value of OWNCLOUD_DB_FAIL

--- latest/overlay/etc/owncloud.d/15-database.sh
+++ latest/overlay/etc/owncloud.d/15-database.sh
@@ -2,30 +2,32 @@
 
 case ${OWNCLOUD_DB_TYPE} in
   "mysql")
-    if ! grep -q ":" <<<${OWNCLOUD_DB_HOST}
+    if ! grep -q ":" <<<"${OWNCLOUD_DB_HOST}"
     then
       OWNCLOUD_DB_HOST=${OWNCLOUD_DB_HOST}:3306
     fi
 
     echo "Waiting for MySQL..."
-    wait-for-it -t ${OWNCLOUD_DB_TIMEOUT} ${OWNCLOUD_DB_HOST}
+    wait_error=false
+    wait-for-it -t "${OWNCLOUD_DB_TIMEOUT}" "${OWNCLOUD_DB_HOST}" || wait_error=true
 
-    if [[ $? -ne 0 && "${OWNCLOUD_DB_FAIL}" == "true" ]]
+    if [[ "${wait_error}" == true && "${OWNCLOUD_DB_FAIL}" == "true" ]]
     then
       echo "Database didn't came up in time!"
       exit 1
     fi
     ;;
   "pgsql")
-    if ! grep -q ":" <<<${OWNCLOUD_DB_HOST}
+    if ! grep -q ":" <<<"${OWNCLOUD_DB_HOST}"
     then
       OWNCLOUD_DB_HOST=${OWNCLOUD_DB_HOST}:5432
     fi
 
     echo "Waiting for PostgreSQL..."
-    wait-for-it -t ${OWNCLOUD_DB_TIMEOUT} ${OWNCLOUD_DB_HOST}
+    wait_error=false
+    wait-for-it -t "${OWNCLOUD_DB_TIMEOUT}" "${OWNCLOUD_DB_HOST}" || wait_error=true
 
-    if [[ $? -ne 0 && "${OWNCLOUD_DB_FAIL}" == "true" ]]
+    if [[ "${wait_error}" == true && "${OWNCLOUD_DB_FAIL}" == "true" ]]
     then
       echo "Database didn't came up in time!"
       exit 1
@@ -36,9 +38,10 @@ esac
 if [[ ${OWNCLOUD_MEMCACHED_ENABLED} == "true" ]]
 then
   echo "Waiting for Memcached..."
-  wait-for-it -t ${OWNCLOUD_MEMCACHED_STARTUP_TIMEOUT} ${OWNCLOUD_MEMCACHED_HOST}:${OWNCLOUD_MEMCACHED_PORT}
+  wait_error=false
+  wait-for-it -t "${OWNCLOUD_MEMCACHED_STARTUP_TIMEOUT}" "${OWNCLOUD_MEMCACHED_HOST}":"${OWNCLOUD_MEMCACHED_PORT}" || wait_error=true
 
-  if [[ $? -ne 0 ]]
+  if [[ "${wait_error}" == true ]]
   then
     echo "Memcached didn't came up in time!"
     exit 1
@@ -48,9 +51,10 @@ fi
 if [[ ${OWNCLOUD_REDIS_ENABLED} == "true" ]] && [[ ${OWNCLOUD_REDIS_SEEDS} == "" ]] && [[ ${OWNCLOUD_REDIS_PORT} != "0" ]]
 then
   echo "Waiting for Redis..."
-  wait-for-it -t ${OWNCLOUD_REDIS_STARTUP_TIMEOUT} ${OWNCLOUD_REDIS_HOST}:${OWNCLOUD_REDIS_PORT}
+  wait_error=false
+  wait-for-it -t "${OWNCLOUD_REDIS_STARTUP_TIMEOUT}" "${OWNCLOUD_REDIS_HOST}:${OWNCLOUD_REDIS_PORT}" || wait_error=true
 
-  if [[ $? -ne 0 ]]
+  if [[ "${wait_error}" == true ]]
   then
     echo "Redis didn't came up in time!"
     exit 1
