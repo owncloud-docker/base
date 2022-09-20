cf6e42004f5f5281f3e806eef0659303f1c4d30b
Robert Kaussow <mail@thegeeklab.de>
2022-09-20 14:41:20 +0200

    refactor: replace su-exec/gosu with setpriv

--- latest/overlay/etc/owncloud.d/35-logging.sh
+++ latest/overlay/etc/owncloud.d/35-logging.sh
@@ -3,7 +3,7 @@
 if [[ ! -e ${OWNCLOUD_LOG_FILE} ]]; then
   if [[ "$(id -u)" == "0" ]]; then
     echo "Touching log file..."
-    su-exec www-data touch "${OWNCLOUD_LOG_FILE}"
+    setpriv --reuid=www-data --regid=www-data --init-groups touch "${OWNCLOUD_LOG_FILE}"
   else
     echo "Touching log file..."
     touch "${OWNCLOUD_LOG_FILE}"
